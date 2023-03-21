9caa38da78738043abf0dafb6197ca85960941b9
Manish Singh <prataponandroid@gmail.com>
2023-03-21 15:16:19 +0100

    fixed the test

--- src/test/java/com/cand/app/integration/APITest.java
+++ src/test/java/com/cand/app/integration/APITest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 public class APITest {
 
     @Autowired
