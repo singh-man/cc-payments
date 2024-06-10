3140166fad9244f7788c44e9da11f36fab5279e3
Manish Singh <prataponandroid@gmail.com>
2024-06-10 13:07:14 +0200

    junit used

--- src/main/java/com/cand/app/repository/ICustomer.java
+++ src/main/java/com/cand/app/repository/ICustomer.java
@@ -3,9 +3,11 @@ package com.cand.app.repository;
 import com.cand.app.entity.Customer;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
+@Repository
 public interface ICustomer extends CrudRepository<Customer, String> {
 
     Customer findByFullName(String customerName);
